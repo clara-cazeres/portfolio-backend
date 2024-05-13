730d92fe90cd55fe50f368e3b3b21452c2edde96
clara-cazeres <claracazeres@gmail.com>
2024-05-13 18:21:33 -0300

    ultimos ajustes

--- modelo/Proyecto.js
+++ modelo/Proyecto.js
@@ -5,19 +5,21 @@ let Schema = mongoose.Schema;
 
 let proyectoSchema = new Schema({
     titulo: { type: String },
-    fecha: { type: Date },
     categoria: { type: String },
-    herramientas: [{ type: String }],
     institucion: { type: String },
     materia: { type: String },
-    sobre: { type: String },
+    fecha: { type: Date },
+    herramientas: [{ type: String }],
+    equipo: [{ type: String }],
+    about: { type: String },
     descripcion: { type: String },
-    colores: [{ type: String }],
-    tipografia: { type: String },
-    imagenes: [{ type: String }],
-    video: { type: String },
+    fondodescripcion: { type: String},
     portada: { type: String },
-    id: { type: Number }
+    logo: { type: String },
+    colores: { type: String },
+    tipografias: { type: String },
+    imagenes: [{ type: String }],
+    video: { type: String }
 })
 
 proyectoSchema.plugin(mongooseUniqueValidator);
